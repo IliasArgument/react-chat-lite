6492e6926e4387ccb5cf09366c520779d0e9d49b
Ilia Koliesnikov <bukovskii@ua.fm>
2022-01-24 17:38:02 +0300

    install app

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import socket from './socket';
+import reducer from './reducer';
+import Login from './components/Login';
+import Chat from './components/Chat';
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    joined: false,
+    roomId: null,
+    userName: null,
+    users: [],
+    messages: [],
+  })
+
+  const onLogin = async (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj
+    });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  }
+
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  const addMessage = (message) => {
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
+  
+  useEffect(() => {
+    socket.on('ROOM:SET_USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', addMessage);
+  }, []);
+
+  window.socket = socket;
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        {!state.joined ? (
+          <Login onLogin={onLogin} />
+        ) : (
+          <Chat {...state} onAddMessage={addMessage} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
